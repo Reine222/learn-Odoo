a1189dc188e103846a9ebca0910cd25646d9f084
Reine222 <koulaireine0222@gmail.com>
2025-04-07 16:38:22 +0000

    Initialisation du projet Odoo avec modules personnalisés

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # learn-Odoo
 apprendre odoo
+
+# installer postgresql
+# cloner le repos de odoo
+# installer le client postgresql
+# creer l'user postgresql et la bd
+# commande pour lancer odoo : 
+python3 odoo-bin --addons-path=addons -d learnodoo
---
+++ customAddons/carpooling/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'carpooling',
+    # 'version': '1.0',
+    # 'category': 'Tools',
+    'description': 'mon premier module.',
+    'author': 'Reine',
+    # 'depends': ['base'],
+    # 'data': ['views/my_model_view.xml'],
+    # 'installable': True,
+    # 'auto_install': False,
+}
